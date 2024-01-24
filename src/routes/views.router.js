0d9590ea2b91a7d1e6e617c79cb95cede6476fea
Matias Delorenzini <matiasdelorenc@gmail.com>
2024-01-23 22:26:30 -0300

    original vertion (ignore past commits)

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,12 +1,7 @@
+import express from 'express'
 import ProductManager from '../classes/ProductManager.js';
-import express from 'express';
-import realtimeRouter from './realtime.router.js';
 const productManager = new ProductManager();
 const router = express.Router()
-
-let products = []
-
-router.use('/realtimeproducts', realtimeRouter);
 
 router.get('/', async (req,res) => {
     const content = await productManager.getProducts();
